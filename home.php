7fe805669c0800ea623889a6c6959bcd37dfcafd
Dinanath Mukhiya <dkmdkm26@gmail.com>
2023-04-18 16:46:15 +0545

    updated layout an login page

---
+++ home.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Bloodspot</title>
+    <link rel="stylesheet" href="index.css" />
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="logo">
+            <img src="logo.png" alt="Bloodspot" />
+        </div>
+        <ul class="nav-links">
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Contact</a></li>
+            <li>
+                <a href="#">Categories</a>
+                <ul class="dropdown">
+                    <li><a href="#">Business</a></li>
+                    <li><a href="#">Children</a></li>
+                    <li><a href="#">Technology</a></li>
+                    <li><a href="#">Fiction</a></li>
+                    <li><a href="#">Toy &amp; Games</a></li>
+                    <li><a href="#">Romance</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Login</a></li>
+            <li class="sign-in"><a href="#">Admin</a></li>
+            <li class="create-account">
+                <a href="#">Exchange Blood</a>
+            </li>
+        </ul>
+        <div class="menu-toggle">
+            <i class="fas fa-bars"></i>
+        </div>
+    </nav>
+
+
+    <section id="banner">
+
+        <!-- blood container section -->
+        <div class="container">
+            <a href="#" class="btn">Donate Now</a>
+        </div>
+        <div class="container">
+            <a href="#" class="btn">Request blood</a>
+        </div>
+
+        <!-- Request blood container -->
+    </section>
+
+    <section id="about">
+        <div class="bloodsection">
+            <div class="bgroup">
+                <a href="#">A-</a>
+                <a href="#">A+</a>
+                <a href="#">AB-</a>
+                <a href="#">AB+</a>
+                <a href="#">B-</a>
+                <a href="#">A+</a>
+                <a href="#">O-</a>
+                <a href="#">O-</a>
+            </div>
+
+
+        </div>
+    </section>
+
+    <footer>
+        <div class="footer-container">
+            <div class="footer-logo">
+                <img src="logo.png" alt="Bloodspot" />
+            </div>
+            <div class="footer-links">
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Categories</a></li>
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Admin</a></li>
+                    <li><a href="#">Exchange Blood</a></li>
+                </ul>
+            </div>
+            <div class="footer-social">
+                <ul>
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; 2023 Bloodspot. All rights reserved.</p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
